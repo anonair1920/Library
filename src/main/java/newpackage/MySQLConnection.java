4f7d5b604e53777ea8a967e48456cff64f0c9067
An Tr <andrecee.tnn@gmail.com>
2021-06-06 01:31:20 +0700

    can't connect to db

--- src/main/java/newpackage/MySQLConnection.java
+++ src/main/java/newpackage/MySQLConnection.java
@@ -5,14 +5,18 @@ import java.sql.DriverManager;
 
 public class MySQLConnection {
     public static Connection connect() {
+        String name,pass,url;
         try {
+            name = "root";
+            pass = "";
+            url = "jdbc:mysql://localost:3360/Library?useSSL=false";
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mysql?user=root&password=");
-            System.out.println("Database connected!");
-            return connection;
-        } catch ( Exception e ) {
+            Connection connection = DriverManager.getConnection(url,name,pass);
+            System.out.println("Database connected!"); 
+            return connection;         
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+        return connect();
     }
 }
